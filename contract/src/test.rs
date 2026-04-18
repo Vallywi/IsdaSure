f3b27da8c069f7195c3454ddf2dc571a7a98653c
Valiwri <jecynvallirieturbanos@gmail.com>
2026-04-17 17:17:21 -1200

    Yeah

--- contract/src/test.rs
+++ contract/src/test.rs
@@ -23,3 +23,38 @@ fn reset_clears_count() {
 
 	assert_eq!(client.get_count(), 0);
 }
+
+#[test]
+fn multiple_increments_accumulate() {
+	let env = Env::default();
+	let contract_id = env.register_contract(None, IsdaSureContract);
+	let client = IsdaSureContractClient::new(&env, &contract_id);
+
+	assert_eq!(client.increment(&2), 2);
+	assert_eq!(client.increment(&3), 5);
+	assert_eq!(client.increment(&4), 9);
+	assert_eq!(client.get_count(), 9);
+}
+
+#[test]
+fn increment_by_zero_keeps_count() {
+	let env = Env::default();
+	let contract_id = env.register_contract(None, IsdaSureContract);
+	let client = IsdaSureContractClient::new(&env, &contract_id);
+
+	client.increment(&7);
+	assert_eq!(client.increment(&0), 7);
+	assert_eq!(client.get_count(), 7);
+}
+
+#[test]
+fn increment_saturates_at_u32_max() {
+	let env = Env::default();
+	let contract_id = env.register_contract(None, IsdaSureContract);
+	let client = IsdaSureContractClient::new(&env, &contract_id);
+
+	let max = u32::MAX;
+	assert_eq!(client.increment(&max), max);
+	assert_eq!(client.increment(&1), max);
+	assert_eq!(client.get_count(), max);
+}
