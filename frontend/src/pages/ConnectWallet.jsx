c599686433253500ab8a83cd469d4a3a99e2e453
Valiwri <jecynvallirieturbanos@gmail.com>
2026-04-20 06:02:11 -1200

    hindi pa tapos

--- frontend/src/pages/ConnectWallet.jsx
+++ frontend/src/pages/ConnectWallet.jsx
@@ -8,6 +8,7 @@ import SpotlightCard from '../components/SpotlightCard';
 import SiteFooter from '../components/SiteFooter';
 import { useWallet } from '../hooks/useWallet';
 
+
 export default function ConnectWallet() {
   const navigate = useNavigate();
   const { connectWallet } = useWallet();
